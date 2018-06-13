8f056533fc08dc9c9622ea34c12d7848a76fb83b
Lukas Kern <lkern.mmt-b2016@fh-salzburg.ac.at>
2018-06-13 13:18:42 +0200

    update

--- src/app.js
+++ src/app.js
@@ -8,23 +8,30 @@ import notFoundTpl from './templates/not-found.hbs'
 import resultTpl from './templates/result.hbs'
 const $app = $('#app')
 
-function index() {
-  $app.html(homeTpl())
+Handlebars.registerHelper('powers', function(powers){
+  return 2018 - parseInt(date.substr(0,4))
+})
+
+function search(){
   $('#solr-query').submit(function() {
-    let helper = $('#title-field').val()
-    helper = helper.split(' ')
+    let input = $('#title-field').val()
+    input = input.split(' ')
     let queryString = ''
-    for (const str of helper) {
-      queryString += `setName_txt_en:${str} OR `
-      queryString += `artist_txt_en:${str} OR `
-      queryString += `name_txt_en:${str} OR `
-      queryString += `text_txt_sort:${str}`
+
+    if (input[0]) { // empty check
+      for (const str of input) {
+        queryString += `setName_txt_en:*${str}* OR artist_txt_en:*${str}* OR name_txt_en:*${str}* OR text_txt_sort:*${str}*`
+      }
     }
-    console.log($('#title-field').val().split(' '));
+    // query mit filter ??
+    //queryString += ` AND cmc_i:2`
+
+    console.log(queryString);
+
 
     let filter = [...filter]
     let queryFilter = ''
-    if (filter[0]) {
+    if (filter[0]) { // empty check
       for (const str of filter) {
         queryFilter += str + ','
       }
@@ -33,7 +40,6 @@ function index() {
     else{
       queryFilter = '*'
     }
-
 
     fetch(`http://127.0.0.1:8983/solr/gettingstarted/select?fl=${queryFilter}&q=${queryString}`)
       .then(res => {
@@ -51,9 +57,15 @@ function index() {
   })
 }
 
+function index() {
+  $app.html(homeTpl())
+  search()
+}
+
 function results(obj){
   console.log(obj.response);
   $app.html(resultTpl(obj.response))
+  search()
 }
 
 function notFound() {
