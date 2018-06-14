da707db081c5a9922b7d10247371a416646b9375
Lukas Kern <lkern.mmt-b2016@fh-salzburg.ac.at>
2018-06-14 12:58:52 +0200

    add: spellchecker

--- src/app.js
+++ src/app.js
@@ -12,18 +12,15 @@ Handlebars.registerHelper('checker', function(data){
   return data < 0 ? '*': data
 })
 
-
+let input
 function search(){
   $('#solr-query').submit(function() {
-    let input = $('#title-field').val()
+    input = $('#title-field').val()
     input = input.split(' ')
     let queryString = ''
 
     if (input[0]) { // empty check
-      for (const str of input) {
-        queryString += `setName_txt_en:*${str}*^1 artist_txt_en:*${str}*^1.5 name_txt_en:*${str}*^2 text_txt_sort:*${str}*^1 `
-        queryString += `setName_txt_en:${str}^1 artist_txt_en:${str}^1.5 name_txt_en:${str}^2 text_txt_sort:${str}^1 `
-      }
+      queryString = creatQueryString(input)
     }
     // query mit filter ??
     let choosen = []
@@ -36,22 +33,37 @@ function search(){
         queryString += color + '+OR+'
       })
       queryString = queryString.substring(0,(queryString.length - 4))
-
     }
     //console.log(queryString);
     input['colors'] = []
     input['colors'] = choosen
-    fetch(`http://127.0.0.1:8983/solr/gettingstarted/select?q=${queryString}&rows=100`)
-      .then(res => {
-        return res.json()
-      })
-      .then(res => {
-        results(res, input)
-      })
-      .catch(err => {
-        console.error(err)
-      })
-      return false
+    // http://127.0.0.1:8983/solr/gettingstarted/spell?q=${queryString}&rows=100
+    // spellchecker http://127.0.0.1:8983/solr/gettingstarted/spell?q=${input}&rows=100
+    request(queryString)
+    return false
+  })
+}
+
+function creatQueryString(inputs){
+  let queryString = ''
+  for (const str of inputs) {
+    queryString += `setName_txt_en:*${str}*^1 artist_txt_en:*${str}*^1.5 name_txt_en:*${str}*^2 text_txt_sort:*${str}*^1 `
+    queryString += `setName_txt_en:${str}^1 artist_txt_en:${str}^1.5 name_txt_en:${str}^2 text_txt_sort:${str}^1 `
+  }
+  console.log(queryString);
+  return queryString
+}
+
+function request(queryString){
+  fetch(`http://127.0.0.1:8983/solr/gettingstarted/spell?q=${queryString}&rows=100`)
+  .then(res => {
+    return res.json()
+  })
+  .then(res => {
+    results(res, input)
+  })
+  .catch(err => {
+    console.error(err)
   })
 }
 
@@ -61,13 +73,20 @@ function index() {
 }
 
 function results(obj, input){
-  console.log(obj.response)
+  console.log(obj)
   $app.html(resultTpl(obj.response))
-  $('h2 > #results-found').text(obj.response.numFound)
-  $('h2 > #query-string').text(input)
-  input['colors'].forEach(el => {
-    $(`.form-check-input[value='${el}']`).prop('checked', true)
-  })
+  if (obj.response.numFound > 0) {
+    $('h2 > #results-found').text(obj.response.numFound)
+    $('h2 > #query-string').text(input)
+    input['colors'].forEach(el => {
+      $(`.form-check-input[value='${el}']`).prop('checked', true)
+    })
+  }
+  else{
+    let word = obj.spellcheck.suggestions[1].suggestion[0].word.split(' ')
+    request(creatQueryString(word))
+  }
+
   search()
 }
 
